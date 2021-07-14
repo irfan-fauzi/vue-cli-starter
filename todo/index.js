d3a362e8057b42384a096dc5433b78d9148ee30b
irfan-fauzi <irfanwintech@yahoo.com>
2021-07-14 20:59:51 +0700

    add computed object

--- todo/index.js
+++ todo/index.js
@@ -11,6 +11,17 @@ const app = Vue.createApp({
         this.dataTodo.push(this.newTodo)
         this.newTodo = ''
       }
+    },
+    hapusItem(){
+        if(this.dataTodo.length){
+
+        }
+    }
+  },
+  // computed : menerima value yang reaktif
+  computed: {
+    todoCount(){
+      return this.dataTodo.length;
     }
   }
 })
