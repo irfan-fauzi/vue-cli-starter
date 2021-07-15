099293ae3a8fcc7c59f8469c026385af5012e9d4
irfan-fauzi <irfanwintech@yahoo.com>
2021-07-15 16:37:52 +0700

    templeate vue cdn

--- new-vue-cli/my-app/src/App.vue
+++ new-vue-cli/my-app/src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>hallo</h1>
+  <div>
+    <h1>{{ myHeading }}</h1>
+    <h2>{{ subTitle }}</h2>
+  </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return{
+      myHeading: "saya adalah vue",
+      subTitle: "hahah ini subtitle"
+    }
+  } 
+}
+</script>
