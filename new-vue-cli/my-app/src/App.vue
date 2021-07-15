cf56a5a6fb4fd0f85a9795ee28399e60ae23236d
irfan-fauzi <irfanwintech@yahoo.com>
2021-07-15 20:11:30 +0700

    set interval as counter vue so easy

--- new-vue-cli/my-app/src/App.vue
+++ new-vue-cli/my-app/src/App.vue
@@ -1,34 +1,51 @@
 <template>
+<main>
   <div>
-    <h1 v-html="myHeading"></h1>
+    <h1 v-if="isShowing">{{ myHeading }}</h1>
+    <h1 v-else >{{ headingCadangan }}</h1>
     <h2>{{ subTitle }}</h2>
-    <ul>
-      <li></li>
+    <h3>{{ count }}</h3>
+  </div>
+  <div>
+    <form v-on:submit.prevent="submitData">
+      <input v-model="newTodo" type="text">
+      <button>add</button>
+    </form>
+    <ul v-for="el in array" v-bind:key="el.id">
+      <li>
+        <p>{{ el }}</p>
+        <button>hapus</button>
+      </li>
     </ul>
   </div>
+</main>
 </template>
 
 <script>
 export default {
   data(){
     return{
-      myHeading: `<h1>Heding</h1>`,
+      myHeading: "Heading",
       subTitle: "hahah ini subtitle",
-      array : [
-        {
-          nama: "irfan fauzi",
-          email: "irfanfauzi@yahoo.com"
-        },
-         {
-          nama: "irfan fauzi",
-          email: "irfanfauzi@yahoo.com"
-        },
-         {
-          nama: "irfan fauzi",
-          email: "irfanfauzi@yahoo.com"
-        }
-      ]
+      headingCadangan: "Vue cadangan",
+      array : [],
+      isShowing: false,
+      newTodo: '',
+      count: 0
     }
-  } 
+  },
+  methods: {
+    submitData(){
+      this.array.push(this.newTodo)
+      this.newTodo = ""
+    }
+  },
+  mounted(){
+    setInterval(() => {
+      if(this.isShowing){
+        this.count++
+      }
+    }, 1000)
+  }  
 }
 </script>
